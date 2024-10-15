db7cb63d0086e17ce0fe7c5431375b807b4805d3
javeedbasha9631 <javidbasha7869@gmail.com>
2024-10-15 10:33:50 +0530

    Update prog1.py

--- prog1.py
+++ prog1.py
@@ -1,1 +1,12 @@
-print("hi")
+code and save the file
+def maximum(a, b):
+    
+    if a >= b:
+        return a
+    else:
+        return b
+    
+# Driver code
+a = 2
+b = 4
+print(maximum(a, b))
